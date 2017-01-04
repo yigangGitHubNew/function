64d4ab98484f80b86891afe5f9bde13bf60a1431
wuyg15871 <wuyg15871@WINDOWS-1J3KD8E.hs.handsome.com.cn>
2017-01-04 10:32:10 +0800

    添加注释

--- tool/src/main/java/com/wuyg/tool/test/HelloWorld.java
+++ tool/src/main/java/com/wuyg/tool/test/HelloWorld.java
@@ -1,5 +1,9 @@
 package com.wuyg.tool.test;
-
+/**
+ * 简单的测试类
+ * @author wuyg15871
+ *
+ */
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
