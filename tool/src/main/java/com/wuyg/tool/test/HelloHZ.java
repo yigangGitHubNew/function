e280cbe0401b6732bc3a3bbb9712ce29ed1d1d4f
wuyg15871 <wuyg15871@WINDOWS-1J3KD8E.hs.handsome.com.cn>
2017-01-04 10:38:47 +0800

    简单的测试杭州的类

--- tool/src/main/java/com/wuyg/tool/test/HelloHZ.java
+++ tool/src/main/java/com/wuyg/tool/test/HelloHZ.java
@@ -1,5 +1,9 @@
 package com.wuyg.tool.test;
-
+/**
+ * 简单的测试类
+ * @author wuyg15871
+ *
+ */
 public class HelloHZ {
 	public static void main(String[] args) {
 		System.out.println("Hello HangZhou");
